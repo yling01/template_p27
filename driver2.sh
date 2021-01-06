3aed9db959a7fe05010ad969a0779d6ed750bc2c
Yingjie Ling <43307135+yling01@users.noreply.github.com>
2021-01-05 21:10:14 -0500

    Update driver2.sh
    Changed cutoff to 90

--- driver2.sh
+++ driver2.sh
@@ -9,31 +9,31 @@ do
     echo "s${i}: Running em2..."
     gmx_mpi grompp -f em2.mdp -c ion.gro -p cx_rsff2_tip3p.top -o em2.tpr &> grompp_em2.log
     gmx_mpi mdrun -v -s em2.tpr -ntomp 4 -deffnm em2 &> mdrun_em2.log
-    python check_trajectory.py --seq ${sequence} --gro em2.gro
+    python check_trajectory.py --seq ${sequence} --gro em2.gro --cutoff 90
     sleep 2
 
     echo "s${i}: Running nvt1..."
     gmx_mpi grompp -v -f nvt1.mdp -c em2.gro -r em2.gro -p cx_rsff2_tip3p.top -o nvt1.tpr &> grompp_nvt1.log
     gmx_mpi mdrun -v -s nvt1.tpr -ntomp 4 -deffnm nvt1 &> mdrun_nvt1.log
-    python check_trajectory.py --seq ${sequence} --gro nvt1.gro
+    python check_trajectory.py --seq ${sequence} --gro nvt1.gro --cutoff 90
     sleep 2
 
     echo "s${i}: Running npt1..."
     gmx_mpi grompp -v -f npt1.mdp -c nvt1.gro -r nvt1.gro -p cx_rsff2_tip3p.top -o npt1.tpr &> grompp_npt1.log
     gmx_mpi mdrun -v -s npt1.tpr -ntomp 4 -deffnm npt1 &> mdrun_npt1.log
-    python check_trajectory.py --seq ${sequence} --gro npt1.gro
+    python check_trajectory.py --seq ${sequence} --gro npt1.gro --cutoff 90
     sleep 2
 
     echo "s${i}: Running nvt2..."
     gmx_mpi grompp -v -f nvt2.mdp -c npt1.gro -p cx_rsff2_tip3p.top -o nvt2.tpr &> grompp_nvt2.log
     gmx_mpi mdrun -v -s nvt2.tpr -ntomp 4 -deffnm nvt2 &> mdrun_nvt2.log
-    python check_trajectory.py --seq ${sequence} --gro nvt2.gro
+    python check_trajectory.py --seq ${sequence} --gro nvt2.gro --cutoff 90
     sleep 2
 
     echo "s${i}: Running npt2..."
     gmx_mpi grompp -v -f npt2.mdp -c nvt2.gro -p cx_rsff2_tip3p.top -o npt2.tpr &> grompp_npt2.log
     gmx_mpi mdrun -v -s npt2.tpr -ntomp 4 -deffnm npt2 &> mdrun_npt2.log
-    python check_trajectory.py --seq ${sequence} --gro npt2.gro
+    python check_trajectory.py --seq ${sequence} --gro npt2.gro --cutoff 90
     sleep 2
 
     cp npt2.gro ../../4bemeta/s${i}
